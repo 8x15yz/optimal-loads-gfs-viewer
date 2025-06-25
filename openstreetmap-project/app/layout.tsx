01aa12dceb57a60e7c1fdc6ebc7381affb8b44d5
unknown <8x15yz@gmail.com>
2025-06-25 18:13:39 +0900

    change source info

--- openstreetmap-project/app/layout.tsx
+++ openstreetmap-project/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'GFS Viewer',
+  description: 'Created with bluemap',
+  generator: 'bluemap',
 }
 
 export default function RootLayout({
